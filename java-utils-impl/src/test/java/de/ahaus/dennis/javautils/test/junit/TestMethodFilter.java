d404c98911290e6f99a9aba2e5d2b82cc14ca158
Dennis Ahaus <dennis.ahaus@gmail.com>
2014-04-21 00:03:18 +0200

    Add new unit test for MethodFilter

---
+++ java-utils-impl/src/test/java/de/ahaus/dennis/javautils/test/junit/TestMethodFilter.java
@@ -0,0 +1,44 @@
+package de.ahaus.dennis.javautils.test.junit;
+
+import static org.junit.Assert.fail;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestMethodFilter {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	public void testAddAnnotationFilter() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddFilter() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFilterClassOfQ() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFilterObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFilterMethodArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAccept() {
+		fail("Not yet implemented");
+	}
+
+}
